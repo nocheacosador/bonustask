c0ddcf09dbd82a6268cfa19ca710c95ac17d72a0
nocheacosador <lukaszaj@gmail.com>
2022-09-29 16:36:03 +0300

    first commit

---
+++ src/main/java/com/bonustask/App.java
@@ -0,0 +1,46 @@
+package com.bonustask;
+
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            Scanner scanner = new Scanner(new File("input.txt"));
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                char[] chars = string.toCharArray();
+                
+                boolean digitBefore = false;
+                int integer = 0;
+                
+                for (char c : chars) {
+                    if ((Character.isDigit(c))) {
+                        if (digitBefore) {
+                            integer = integer * 10 + Character.getNumericValue(c);
+                            if (integer >= 10) {
+                                System.out.println(string);
+                                break;
+                            }
+                        }
+                        else {
+                            integer = Character.getNumericValue(c);
+                        }
+                        digitBefore = true;
+                    }
+                    else {
+                        digitBefore = false;
+                    }
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
